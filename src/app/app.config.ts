8da159cb916aa9c993a3435abb13c853c16c830a
Alexander Boldt <boldt.alexander@outlook.com>
2026-05-01 15:16:01 +0200

    added first keycloak integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideKeycloak } from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideKeycloak({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'musiccollection',
+        clientId: 'musiccollection-client'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        pkceMethod: 'S256'
+      }
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('musiccollection-frontend');
+
+  private readonly keycloak = inject(Keycloak);
+
+  ngOnInit() {
+    this.keycloak.loadUserProfile().then(profile => {
+      this.title.set(profile.firstName ?? "anonymous");
+    });
+  }
 }
