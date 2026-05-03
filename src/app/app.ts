4db4bbc699c460bb219759889c91ae173f32a812
Alexander Boldt <boldt.alexander@outlook.com>
2026-05-03 11:54:03 +0200

    moved the create artist section into a separate component

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,29 +7,32 @@ import { Observable } from 'rxjs';
 import { Api } from './api';
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatButton} from '@angular/material/button';
-import { MatFormField, MatLabel, MatInput } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { User } from './user';
+import { ArtistCreate } from './component/artist_create';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe, MatCard, MatCardHeader, MatCardContent, MatCardActions, MatButton, MatCardTitle, MatFormField, MatLabel, MatInput, FormsModule, User],
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    MatCardTitle,
+    FormsModule,
+    User,
+    ArtistCreate
+  ],
   template: `
     <div id="content">
       <user />
 
       <h1>Music Collection</h1>
 
-      <mat-card id="cardCreateArtist" appearance="outlined">
-        <h2>Create an artist</h2>
-        <form>
-          <mat-form-field>
-            <mat-label>Artist</mat-label>
-            <input matInput type="text" [(ngModel)]="inputCreateArtist" name="inputCreateArtist">
-          </mat-form-field>
-          <button type="button" (click)="createArtist(inputCreateArtist)" [disabled]="inputCreateArtist.length == 0" matButton="filled">CREATE</button>
-        </form>
-      </mat-card>
+      <artist-create (created)="readAllArtist()" />
 
       <div id="artistContent">
         @for (artist of artists(); track artist.id) {
@@ -59,8 +62,6 @@ export class App {
 
   imageUrlMap = new Map<number, Observable<string>>();
 
-  inputCreateArtist = ''
-
   private readonly keycloak = inject(Keycloak);
   private readonly http = inject(HttpClient);
 
@@ -68,13 +69,6 @@ export class App {
 
   ngOnInit() {
     this.readAllArtist()
-  }
-
-  createArtist(name: string) {
-    this.api.createArtist(name).subscribe(data => {
-      this.inputCreateArtist = ''
-      this.readAllArtist();
-    })
   }
 
   readAllArtist() {
---
+++ src/app/component/artist_create.ts
@@ -0,0 +1,61 @@
+import { Component, inject, input, output } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import Keycloak from 'keycloak-js';
+import { HttpClient } from '@angular/common/http';
+import { Api } from '../api';
+
+@Component({
+  selector: 'artist-create',
+  imports: [
+    MatButton,
+    MatCard,
+    FormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel
+  ],
+  template: `
+    <mat-card id="cardCreateArtist" appearance="outlined">
+      <h2>Create Artist</h2>
+      <form>
+        <mat-form-field>
+          <mat-label>Artist</mat-label>
+          <input matInput type="text" [(ngModel)]="inputCreateArtist" name="inputCreateArtist">
+        </mat-form-field>
+        <button type="button" (click)="createArtist(inputCreateArtist)" [disabled]="inputCreateArtist.length == 0" matButton="filled">CREATE</button>
+      </form>
+    </mat-card>
+  `,
+  styles: `
+    #cardCreateArtist {
+      padding: 16px;
+      text-align: center;
+    }
+
+    #cardCreateArtist mat-form-field {
+      margin-right: 16px;
+    }
+  `
+})
+export class ArtistCreate {
+  title = input<string>();
+
+  inputCreateArtist = ''
+
+  created = output<boolean>();
+
+  private readonly keycloak = inject(Keycloak);
+  private readonly http = inject(HttpClient);
+
+  private api = new Api(this.http, this.keycloak);
+
+  createArtist(name: string) {
+    this.api.createArtist(name).subscribe(data => {
+      this.inputCreateArtist = ''
+      this.created.emit(true)
+    })
+  }
+}
