0973a95f63173571a5f64385f23f9ed6e533ec3f
Alexander Boldt <boldt.alexander@outlook.com>
2026-05-04 09:28:33 +0200

    created artist component to combine creation and overview

---
+++ src/app/component/artist.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+import { ArtistCreate } from './artist_create';
+import { ArtistReadAll } from './artist_read_all';
+
+@Component({
+  selector: 'artist',
+  imports: [
+    ArtistCreate,
+    ArtistReadAll
+  ],
+  template: `
+    <artist-create (created)="artistCreated()" />
+    <artist-read-all [reloadArtists]="reloadArtists()" />
+  `,
+  styles: `
+  `
+})
+export class Artist {
+  reloadArtists = signal(false)
+
+  artistCreated() {
+    this.reloadArtists.update(value => !value)
+  }
+}
